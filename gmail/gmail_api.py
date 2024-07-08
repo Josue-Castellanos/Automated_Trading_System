1584afe1ce5f7ab3ca16e11fabdce36cf0250a5f
Josue Castellanos <jcastellanos@horizon.csueastbay.edu>
2024-07-08 00:51:09 -0700

    new Gmail class

--- gmail/gmail_api.py
+++ gmail/gmail_api.py
@@ -1,11 +1,6 @@
-# Created by Google https://github.com/googleworkspace/python-samples/blob/main/gmail/quickstart/quickstart.py
-# Edited by https://josuecastellanos.com
-
-# Imports
 import time
 import os.path
 import threading
-import trading_bot
 from pathlib import Path
 from dotenv import load_dotenv
 from googleapiclient.discovery import build
@@ -13,124 +8,109 @@ from google.oauth2.credentials import Credentials
 from google.auth.transport.requests import Request
 from google_auth_oauthlib.flow import InstalledAppFlow
 
-# Load sensitive Information
-load_dotenv(dotenv_path=Path('./gmail/app_info/.env'))
-CLIENT_ID = os.getenv("clientId")
-CLIENT_SECRET = os.getenv("clientSecret")
 
-# URI & Paths
-READ_SCOPES = ["https://www.googleapis.com/auth/gmail.readonly"] # If modifying these scopes, delete the file token.json.
-MOD_SCOPES = ["https://www.googleapis.com/auth/gmail.modify"]
+class Gmail:
+    def __init__(self, log_signal=None):
+        self._load_env()
+        self.creds = None
+        self.current_position = None
+        self.CALLEVENT = threading.Event()
+        self.PUTEVENT = threading.Event()
+        self.log_signal = log_signal
 
-# Event
-CALLEVENT = threading.Event()
-PUTEVENT = threading.Event()
+    def _load_env(self):
+        load_dotenv(dotenv_path=Path('./gmail/app_info/.env'))
+        self.CLIENT_ID = os.getenv("clientId")
+        self.CLIENT_SECRET = os.getenv("clientSecret")
+        self.READ_SCOPES = ["https://www.googleapis.com/auth/gmail.readonly"]
+        self.MOD_SCOPES = ["https://www.googleapis.com/auth/gmail.modify"]
 
-class tokens:
-    creds = None
-    token = None
-    refreshToken = None
-    account = None
-    expiry = None
-    # clientId = None
-    # clientSecret = None
+    def initialize(self):
+        self._check_keys()
+        self._token_manager("init")
 
-    
-def initialize():
-    # Check the client id and client secret
-    _CheckKeys()
-    # Set credentials and tokens
-    _TokenManager("init")
-    # Check inbox (ADD Daemon for checking mail thread)
-    #threading.Thread(target=trading_bot.eventHandler, args=(CALLEVENT, PUTEVENT)).start()
+    def _check_keys(self):
+        if len(self.CLIENT_ID) != 72 or len(self.CLIENT_SECRET) != 35:
+            raise ValueError("Incorrect keys or no keys found, add keys in app_info/.env")
 
-    # Check inbox (ADD Daemon for checking mail thread)
-    threading.Thread(target=trading_bot.callEventHandler, args=(CALLEVENT,)).start()
-    threading.Thread(target=trading_bot.putEventHandler, args=(PUTEVENT,)).start()
-    
+    def _refresh_token(self, creds_path):
+        if self.creds and self.creds.expired and self.creds.refresh_token:
+            self.creds.refresh(Request())
+        else:
+            flow = InstalledAppFlow.from_client_secrets_file(creds_path, self.MOD_SCOPES)
+            self.creds = flow.run_local_server(port=0)
+        self._token_manager("set", self.creds)
 
-def _CheckKeys():
-  if len(CLIENT_ID) != 72 or len(CLIENT_SECRET) != 35:
-        print("Incorrect keys or no keys found, add keys in app_info/.env")
-        quit()
+    def _token_manager(self, todo=None, credentials=None):
+        credentials_path = './gmail/app_info/credentials.json'
+        token_path = './gmail/app_info/token.json'
 
-
-def _RefreshToken(credsPath):
-    if tokens.creds and tokens.creds.expired and tokens.creds.refresh_token:
-        tokens.creds.refresh(Request())
-    else:
-        flow = InstalledAppFlow.from_client_secrets_file(credsPath, MOD_SCOPES)
-        tokens.creds = flow.run_local_server(port=0)
-    _TokenManager("set", tokens.creds)
-
-
-def _TokenManager(todo=None, credentials=None):
-    credentialsPath = './gmail/app_info/credentials.json'
-    tokenPath = './gmail/app_info/token.json'
-
-    def writeTokenVar(tokenCreds):
-        tokens.creds = tokenCreds
-        # tokens.token = tokenCreds.get("token")
-        # tokens.refreshToken = tokenCreds.get("refresh_token")
-        # tokens.account = tokenCreds.get("account")
-        # tokens.expiry = tokenCreds.get("expiry")
-
-    def writeTokenFile(newCredentials):
-        with open(tokenPath, "w") as token:
-            token.write(newCredentials.to_json()) 
-
-    def readFromTokenFile():
-        return Credentials.from_authorized_user_file(tokenPath, MOD_SCOPES)
-    
-    try:
         if todo == "set":
             if credentials is not None:
-                writeTokenFile(credentials)
-                writeTokenVar(credentials)
+                with open(token_path, "w") as token:
+                    token.write(credentials.to_json())
+                self.creds = credentials
             else:
-                print("Error in setting token file, null values given")
+                raise ValueError("Error in setting token file, null values given")
         elif todo == "init":
-            creds = readFromTokenFile()
-            writeTokenVar(creds)
-            if not creds or not creds.valid:
-                raise RuntimeError("ERROR: No valid credentials available.")
-    except Exception as e:
-        _RefreshToken(credentialsPath)
-            
+            try:
+                self.creds = Credentials.from_authorized_user_file(token_path, self.MOD_SCOPES)
+                if not self.creds or not self.creds.valid:
+                    raise RuntimeError("ERROR: No valid credentials available.")
+            except Exception:
+                self._refresh_token(credentials_path)
 
-def _CheckInbox(CALLEVENT, PUTEVENT):
-    # Call the Gmail API
-    service = build("gmail", "v1", credentials=tokens.creds)
-    # Retreive a list from 'me' gmail in my INBOX folder of all unread queries
-    results = service.users().messages().list(userId="me", labelIds=["INBOX"], q="is:unread").execute()
-    # Get 'message' item from the query objects in list
-    signals = results.get("messages", [])
+    def _check_inbox(self):
+        service = build("gmail", "v1", credentials=self.creds)
+        results = service.users().messages().list(userId="me", labelIds=["INBOX"], q="is:unread").execute()
+        signals = results.get("messages", [])
 
-    if not signals:
-        pass
-    else:
-        # Iterate through list
+        if not signals:
+            return
+
         for signal in signals[:1]:
-            # Retrieve query from list of UNREAD queries
             message = service.users().messages().get(userId="me", id=signal["id"]).execute()
-            # Mark query as READ
             service.users().messages().modify(userId="me", id=signal["id"], body={"removeLabelIds": ["UNREAD"]}).execute()
-            # Display 'message' item
-            print(message['snippet'][36:42] + "\n")
+            
+            signal_type = message['snippet'][36:42].upper()
+            
+            if signal_type in ('CALL5-', 'CALL15', 'CALL30', 'CALL1H', 'CALL2H', 'CALL4H', 'C5----', 'C15---', 'C30---', 'C1H---', 'C2H---', 'C4H---'):
+                if self.current_position != 'CALL' and not self.CALLEVENT.is_set():
+                    self.current_position = 'CALL'
+                    self.CALLEVENT.set()
+                    self.PUTEVENT.clear()
+                    self.log_signal.emit(f"CALL signal received: {signal_type}")
+            elif signal_type in ('PUT5--', 'PUT15-', 'PUT30-', 'PUT1H-', 'PUT2H-', 'PUT4H-', 'P5----', 'P15---', 'P30---', 'P1H---', 'P2H---', 'P4H---'):
+                if self.current_position != 'PUT' and not self.PUTEVENT.is_set():
+                    self.current_position = 'PUT'
+                    self.PUTEVENT.set()
+                    self.CALLEVENT.clear()
+                    self.log_signal.emit(f"PUT signal received: {signal_type}")
 
-            if message.get('snippet')[36:42] in ('CALL5-', 'CALL15', 'CALL30', 'CALL1H', 'CALL2H', 'CALL4H', 'Call5-', 'Call15', 'Call30', 'Call1h', 'Call2h', 'Call4h', 'C5----', 'C15---', 'C30---', 'C1H---', 'C2H---', 'C4H---'):
-                if not CALLEVENT.is_set():
-                    CALLEVENT.set()
-            elif message.get('snippet')[36:42] in ('PUT5--', 'PUT15-', 'PUT30-', 'PUT1H-', 'PUT2H-', 'PUT4H-', 'Put5--', 'Put15-', 'Put30-', 'Put1h-', 'Put2h-', 'Put4h-', 'P5----', 'P15---', 'P30---', 'P1H---', 'P2H---', 'P4H---'):
-                if not PUTEVENT.is_set():
-                    PUTEVENT.set()
-            else:
-                pass
+    def check_email_automatic(self):    
+        def checker():
+            while True:
+                try:
+                    self._check_inbox()
+                except Exception as e:
+                    self.log_signal.emit(f"Error checking inbox: {e}")
+                time.sleep(1)
+        threading.Thread(target=checker, daemon=True).start()
 
+    def get_current_position(self):
+        return self.current_position
 
-def checkEmailAutomatic(CALLEVENT, PUTEVENT):
-    def checker():
-        while True:
-            _CheckInbox(CALLEVENT, PUTEVENT)
-            time.sleep(1)
-    threading.Thread(target=checker, daemon=True).start()
+    def reset_position(self):
+        self.current_position = None
+        self.CALLEVENT.clear()
+        self.PUTEVENT.clear()
+
+    def wait(self):
+        self.CALLEVENT.wait()
+        self.PUTEVENT.wait()
+        
+    def get_call_event(self):
+        return self.CALLEVENT
+
+    def get_put_event(self):
+        return self.PUTEVENT
