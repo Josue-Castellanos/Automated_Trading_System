062d145534151e76f3599d49b9f77103ef55a2fc
Josue Castellanos <jcastellanos@horizon.csueastbay.edu>
2025-03-31 01:03:49 -0700

    Moved utility functions to utils

--- app/utils.py
+++ app/utils.py
@@ -35,28 +35,123 @@ def convert_epoch_to_datetime(epoch_ms):
     return datetime_adjusted
 
 
-def create_candle_dataframe(candles):
+def create_order(price, symbol, type, position_size, var='OPEN'):
+    """
+    Create an order dictionary for buying or selling a position.
+
+    Args:
+        price (float): The price at which to place the order.
+        symbol (str): The symbol of the instrument to trade.
+        type (str): The type of order ('BUY' or 'SELL').
+        var (str, optional): The variability of the order ('OPEN' or 'CLOSE'). Defaults to 'OPEN'.
+
+    Returns:
+        dict: A dictionary representing the order details.
+    """
+    if type != 'BUY':
+        var='CLOSE'
+
+    order = {
+        'orderType': 'LIMIT',
+        'session': 'NORMAL',
+        'price': price,
+        'duration': 'DAY',
+        'orderStrategyType': 'SINGLE',
+        'orderLegCollection': [{
+            'instruction': f'{type}_TO_{var}',
+            'quantity': position_size,
+            'instrument': {
+                'symbol': symbol,
+                'assetType': 'OPTION'
+            }
+        }]
+    }
+    return order
+
+
+def create_option_dataframe(options):
+    """
+    
+    """
+    data = []
+    exp_date_map = options.get('callExpDateMap') or options.get('putExpDateMap')
+    for exp_date, strikes in exp_date_map.items():
+        for strike, options_list in strikes.items():
+            for option in options_list:
+                option_data = {
+                    'Put/Call': option.get('putCall'),
+                    'Symbol': option.get('symbol'),
+                    'Description': option.get('description'),
+                    'Bid': option.get('bid'),
+                    'Ask': option.get('ask'),
+                    'Volume': option.get('totalVolume'),
+                    'Delta': option.get('delta'),
+                    'OI': option.get('openInterest'),
+                    'Expiration': exp_date,
+                    'Strike': strike,
+                    'ITM': option.get('inTheMoney')
+                }
+                data.append(option_data)
+    return pd.DataFrame(data)
+
+
+def create_candle_dataframe(candles, freq):
     """Convert API response to a DataFrame."""
-    data = [
-        {
-            'Datetime': convert_epoch_to_datetime(ohlcv['datetime']),
-            'Open': ohlcv.get('open'),
-            'High': ohlcv.get('high'),
-            'Low': ohlcv.get('low'),
-            'Close': ohlcv.get('close'),
-            'Volume': ohlcv.get('volume')
-        }
-        for ohlcv in candles['candles'] if ohlcv.get('datetime') is not None
-    ]
-    df = pd.DataFrame(data)
+    data = []
+    aggregated_data = []
+
+    if freq in [1, 5, 10, 15, 30]:
+        aggregated_data = [
+            {
+                'Datetime': convert_epoch_to_datetime(ohlcv['datetime']),
+                'Open': ohlcv.get('open'),
+                'High': ohlcv.get('high'),
+                'Low': ohlcv.get('low'),
+                'Close': ohlcv.get('close'),
+                'Volume': ohlcv.get('volume')
+            }
+            for ohlcv in candles['candles'] if ohlcv.get('datetime') is not None
+        ]
+    else:
+        for ohlcv in candles['candles']:
+            if ohlcv.get('datetime') is None:
+                continue
+
+            new_candle = {
+                'Datetime': convert_epoch_to_datetime(ohlcv['datetime']),
+                'Open': ohlcv.get('open'),
+                'High': ohlcv.get('high'),
+                'Low': ohlcv.get('low'),
+                'Close': ohlcv.get('close'),
+                'Volume': ohlcv.get('volume')
+            }
+
+            data.append(new_candle)
+
+            # Check if we have candles to aggregate
+            if len(data) == freq:
+                aggregated_candle = {
+                    'Datetime': data[0]["Datetime"],   # Timestamp of first candle in the group
+                    'Open': data[0]["Open"],           # Open price from first candle
+                    'High': max(candle["High"] for candle in data),
+                    'Low': min(candle["Low"] for candle in data),
+                    'Close': data[-1]["Close"],        # Close price from last candle
+                    'Volume': sum(candle["Volume"] for candle in data),
+                }
+                aggregated_data.append(aggregated_candle)
+                data.clear()  # Reset for next batch
+    df = pd.DataFrame(aggregated_data)
     df.set_index('Datetime', inplace=True)
     return df
 
 
-def fetch_price_data(schwab, symbol, start, end):
+def fetch_price_data(schwab, symbol, time, freq, start, end):
     """Fetch price history from Schwab API."""
-    response = schwab.price_history(symbol, 'day', 1, 'minute', 5, start, end, True, True)
-    df = create_candle_dataframe(response.json()) if response.ok else None
+    if freq in [1, 5, 10, 15, 30]:
+        response = schwab.price_history(symbol, 'day', 1, time, freq, start, end, True, True)
+    else:
+        response = schwab.price_history(symbol, 'day', 1, time, 1, start, end, True, True)
+    df = create_candle_dataframe(response.json(), freq) if response.ok else None
     # df.to_csv("/Users/josuecastellanos/Documents/Automated_Trading_System/market.csv", mode='w', index=True)
     return df
 
