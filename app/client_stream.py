4b6c93a301a29efe15394282b634b865a164e65d
Josue Castellanos <jcastellanos@horizon.csueastbay.edu>
2025-04-11 07:27:06 -0700

    minor changes

--- app/client_stream.py
+++ app/client_stream.py
@@ -177,7 +177,7 @@ class Client:
 
             # Good Contract ranges
             exp_contracts = strike_price_df.iloc[:expected_move_round + 1][::-1]
-            roi_contracts = strike_price_df.loc[(strike_price_df['ROI'] <= 3000) & (strike_price_df['Delta'] >= 0.05) & (strike_price_df['Ask'] >= 0.10)][::-1]
+            roi_contracts = strike_price_df.loc[(strike_price_df['ROI'] <= 5000) & (strike_price_df['Delta'] >= 0.02) & strike_price_df['Ask'] >= 0.10][::-1]
             ask_contracts = strike_price_df.loc[strike_price_df['Ask'] <= self.contract_price]
 
             # RAISE EXCEPTION: If dataframe is empty
